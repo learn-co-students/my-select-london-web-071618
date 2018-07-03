406cc091045e2182b404d48a009c2a31966661aa
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-03 16:01:50 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    selected = []
+    i = 0
+    while i<collection.length
+      if (yield collection[i]) === true
+        selected << collection[i]
+      end 
+      i += 1
+    end
+  else
+    nil
+  end
+  selected
 end
